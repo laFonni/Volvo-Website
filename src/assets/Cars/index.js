9e2836fd13b67d4262a378f0cbd8b4e5c5b3f667
Olaf Grykalowski <olaf.grykalowski@gmail.com>
2023-10-28 00:11:29 +0200

    feat: carPics to files

--- src/assets/Cars/index.js
+++ src/assets/Cars/index.js
@@ -1,13 +1,46 @@
-import C40_Recharge from "./C40-recharge.png";
-import XC40_Recharge from "./XC40-recharge.png";
-import EX30 from "./EX30.png";
-import EX90 from "./EX90.png";
-
+import C40_Recharge from "./c40-recharge.webp";
+import XC40_Recharge from "./xc40-recharge.webp";
+import EX30 from "./ex30.webp";
+import EX90 from "./ex90.webp";
+import XC90_Hybrid from "./xc90-hybrid.webp";
+import XC60_Hybrid from "./xc60-hybrid.webp";
+import S90_Hybrid from "./s90-hybrid.webp";
+import S60_Hybrid from "./s60-hybrid.webp";
+import V90_Hybrid from "./v90-hybrid.webp";
+import V60_Hybrid from "./v60-hybrid.webp";
+import XC90 from "./xc90.webp";
+import XC60 from "./xc60.webp";
+import XC40 from "./xc40.webp";
+import S90 from "./s90.webp";
+import S60 from "./s60.webp";
+import V90 from "./v90.webp";
+import V60 from "./v60.webp";
+import V90_C_C from "./v90-cross-country.webp";
+import V60_C_C from "./v60-cross-country.webp";
 
 
 export {
+    
     C40_Recharge,
     XC40_Recharge,
     EX30,
-    EX90
+    EX90,
+
+    XC90_Hybrid,
+    XC60_Hybrid,
+    S90_Hybrid,
+    S60_Hybrid,
+    V90_Hybrid,
+    V60_Hybrid,
+
+    XC90,
+    XC60,
+    XC40,
+    S90,
+    S60,
+    V90,
+    V60,
+    V90_C_C,
+    V60_C_C,
+
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ import ourCarsCard from "../components/navCards/OurCarsCard";
 import purchaseCard from "../components/navCards/PurchaseCard";
 import forOwnersCard from "../components/navCards/ForOwnersCard";
 import aboutUsCard from "../components/navCards/AboutUsCard";
-import { C40_Recharge, XC40_Recharge, EX30, EX90 } from "../assets/Cars";
+import { C40_Recharge, XC40_Recharge, EX30, EX90, XC60_Hybrid, XC90, XC90_Hybrid, S90, S90_Hybrid, S60_Hybrid, V90_Hybrid, V60_Hybrid, XC60, XC40, S60, V90, V60, V90_C_C, V60_C_C } from "../assets/Cars";
 
 export const navCards = [
   { key: "ourCars", label: "Nasze samochody", card: ourCarsCard },
@@ -64,37 +64,37 @@ export const hybridCars = [
     class: "SUV",
     model: "XC90",
     price: "379 900",
-    image: EX90,
+    image: XC90_Hybrid,
   },
   {
     class: "SUV",
     model: "XC60",
     price: "264 900",
-    image: EX90,
+    image: XC60_Hybrid,
   },
   {
     class: "Sedan",
     model: "S90 Recharge",
     price: "323 900",
-    image: EX90,
+    image: S90_Hybrid,
   },
   {
     class: "Sedan",
     model: "S60 Recharge",
     price: "349 900",
-    image: EX90,
+    image: S60_Hybrid,
   },
   {
     class: "Kombi",
     model: "V90",
     price: "299 900",
-    image: EX90,
+    image: V90_Hybrid,
   },
   {
     class: "Kombi",
     model: "V60",
     price: "244 900",
-    image: EX90,
+    image: V60_Hybrid,
   },
 ];
 
@@ -103,54 +103,54 @@ export const mildHybridCars = [
     class: "SUV",
     model: "XC90",
     price: "334 900",
-    image: EX90,
+    image: XC90,
   },
   {
     class: "SUV",
     model: "XC60",
     price: "219 900",
-    image: EX90,
+    image: XC60,
   },
   {
     class: "SUV",
     model: "XC40",
     price: "164 900",
-    image: EX90,
+    image: XC40,
   },
   {
     class: "Sedan",
     model: "S90",
     price: "270 900",
-    image: EX90,
+    image: S90,
   },
   {
     class: "Sedan",
     model: "S60",
     price: "247 900",
-    image: EX90,
+    image: S60,
   },
   {
     class: "Kombi",
     model: "V90",
     price: "259 900",
-    image: EX90,
+    image: V90,
   },
   {
     class: "Kombi",
     model: "V60",
     price: "169 900",
-    image: EX90,
+    image: V60,
   },
   {
     class: "Kombi",
     model: "V90 Cross Country",
     price: "314 600",
-    image: EX90,
+    image: V90_C_C,
   },
   {
     class: "Kombi",
     model: "V60 Cross Country",
     price: "238 900",
-    image: EX90,
+    image: V60_C_C, 
   },
 ];
