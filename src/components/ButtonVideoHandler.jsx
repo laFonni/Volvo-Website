62f4e6e034e5b6ba0ed4a88f45ee7c50643b07b2
Olaf Grykalowski <olaf.grykalowski@gmail.com>
2023-11-09 15:08:07 +0100

    feat: EX30 Link on Video

--- src/components/ButtonVideoHandler.jsx
+++ src/components/ButtonVideoHandler.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 
 const ButtonVideoHandler = ({handler, icon}) => {
   return (
-    <button onClick={handler} className="absolute bottom-10 right-10 hover:bg-gray-100 hover:rounded-full p-1">
+    <button onClick={handler} className="absolute bottom-5 right-5 bg-neutral-300 rounded-full p-1">
           <img src={icon} alt="pause" height={32} width={32} />
     </button>
     
