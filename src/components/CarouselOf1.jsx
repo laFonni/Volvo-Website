6afcba43419f042c827545f84d687ad86aefb6ff
Olaf Grykalowski <olaf.grykalowski@gmail.com>
2023-12-10 13:44:32 +0100

    fix: placement of carousels

---
+++ src/components/CarouselOf1.jsx
@@ -0,0 +1,57 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import CarouselCarCard from "./CarouselCarCard";
+import PropTypes from "prop-types";
+
+import { AllCars } from "../constants";
+
+
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 2,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+const CarouselOf1 = ({activeClassOfCar}) => {
+
+  let classOfCar = AllCars.filter((car) => car.classOfCar === activeClassOfCar)
+
+  CarouselOf1.propTypes = {
+    activeClassOfCar: PropTypes.string.isRequired,
+  };
+
+  return (
+    <div className= 'marginCardsSidesCenter1 relative ' >
+      <Carousel
+        responsive={responsive}
+        partialVisible={false}
+        draggable={false}
+        className="pb-24"
+        transitionDuration={100}
+      >
+        {classOfCar.map((car) => (
+          <CarouselCarCard
+            key={car.model}
+            classOfCar={car.classOfCar}
+            model={car.model}
+            price={car.price}
+            type={car.type}
+            image={car.image}
+          />
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default CarouselOf1;
--- src/sections/CaruselOfCars.jsx
+++ src/sections/CaruselOfCars.jsx
@@ -1,44 +1,37 @@
-import 'react-multi-carousel/lib/styles.css';
-import CarouselOfAllCars from '../components/navCards/CarouselOfAllCars';
-import { useState } from 'react';
-
-
+import "react-multi-carousel/lib/styles.css";
+import CarouselOf4AndLess from "../components/CarouselOf4AndLess";
+import CarouselOfAbove4 from "../components/CarouselOfAbove4";
+import { useState } from "react";
+import CarouselOf1 from "../components/CarouselOf1";
 
 const routes = {
-  AllCars: <CarouselOfAllCars />,
-  SUV: null,
-  Crossover: <CarouselOfAllCars/>,
-  Sedan: <CarouselOfAllCars/>,
-  Kombi: <CarouselOfAllCars/>,
-
-
+  AllCars: <CarouselOfAbove4 activeClassOfCar={'AllCars'}/>,
+  SUV: <CarouselOfAbove4 activeClassOfCar={'SUV'}/>,
+  Crossover: <CarouselOf1 activeClassOfCar={'Crossover'}/>,
+  Sedan: <CarouselOf4AndLess activeClassOfCar={'Sedan'}/>,
+  Kombi: <CarouselOf4AndLess activeClassOfCar={'Kombi'}/>
 };
 
 const CaruselOfCars = () => {
-  const [activeCarousel, setActiveCarousel] = useState('AllCars');
+  const [activeCarousel, setActiveCarousel] = useState("AllCars");
 
-  
-  
+  return (
+    <div className="flex flex-col pt-20 bg-red-100  relative">
+      <div className="flex justify-center text-4xl font-semibold pb-6">
+        Wszystie modele Recharge
+      </div>
 
+      <div className="flex flex-row text-gray-600 gap-5 justify-center pb-6 ">
+        {Object.keys(routes).map((key) => (
+          <button onClick={() => setActiveCarousel(key)} key={key}>
+            {key}
+          </button>
+        ))}
+      </div>
 
-    return (
-        <div className='flex flex-col pt-20 bg-red-100  relative'>
-            <div className='flex justify-center text-4xl font-semibold pb-6'>
-                Wszystie modele Recharge
-            </div>
+      {routes[activeCarousel]}
+    </div>
+  );
+};
 
-            <div className='flex flex-row text-gray-600 gap-5 justify-center pb-6 '>
-            {Object.keys(routes).map((key) => (<button  onClick={() => setActiveCarousel(key)} key={key}>{key}</button>))}
-
-            </div>
-            
-           
-
-            {routes[activeCarousel]}
-            
-            
-        </div>
-    )
-}
-
-export default CaruselOfCars
+export default CaruselOfCars;
